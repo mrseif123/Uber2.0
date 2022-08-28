85fcdaf9df3b0142471c0f19ae6c90bfc11b5b29
Seaf Aliyan <codo47@gmail.com>
2022-08-28 14:41:35 +0300

    added navigation on map

--- components/Map.js
+++ components/Map.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import MapView, { Marker } from "react-native-maps";
-import { selectOrigin } from "../slices/navSlice";
+import { selectDestination, selectOrigin } from "../slices/navSlice";
 import { useSelector } from "react-redux";
+import MapViewDirections from "react-native-maps-directions";
+import { GOOGLE_MAPS_API_KEY } from "@env";
 
 const Map = () => {
   const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: { top: 50, bottom: 50, left: 50, right: 50 },
+    });
+  }, [origin, destination]);
+
   return (
     <MapView
+      ref={mapRef}
       style={{ flex: 1 }}
       mapType="mutedStandard"
       initialRegion={{
@@ -16,6 +29,15 @@ const Map = () => {
         longitudeDelta: 0.005,
       }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_API_KEY}
+          strokeWidth={6}
+          strokeColor="black"
+        />
+      )}
       {origin?.location && (
         <Marker
           coordinate={{
@@ -25,6 +47,18 @@ const Map = () => {
           title="Origin"
           description={origin.description}
           identifier="origin"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate={{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title="Destination"
+          description={destination.description}
+          identifier="destination"
         />
       )}
     </MapView>
