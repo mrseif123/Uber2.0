6457a7f924081ad6a0efcf53bac9ec7d67c1fb7d
mrseif123 <seaf.aliyan@mail.huji.ac.il>
2022-09-02 01:30:21 +0300

    added route to favourties function

--- components/Map.js
+++ components/Map.js
@@ -16,6 +16,7 @@ const Map = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     if (!origin || !destination) return;
+    console.log(destination);
     mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
       edgePadding: { top: 50, bottom: 50, left: 50, right: 50 },
     });
